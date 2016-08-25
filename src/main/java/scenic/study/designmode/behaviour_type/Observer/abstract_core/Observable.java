7280a416a6d16baff629b11af6180eb17232bc7c
liuzd <907245015@qq.com>
2016-08-25 16:02:40 +0800

    rename package

--- src/main/java/scenic/study/designmode/behaviour_type/Observer/abstract_core/Observable.java
+++ src/main/java/scenic/study/designmode/behaviour_type/Observer/abstract_core/Observable.java
@@ -1,18 +1,17 @@
-package scenic.study.designmode.behaviour_type.Observer.abstract_core;
+package scenic.study.designmode.behaviour_type.observer.abstract_core;
 
 import java.util.ArrayList;
 import java.util.List;
-
 /**
  * Created by scenic on 16/8/10.
  */
 public abstract class Observable {
-    List<Observer> mObjservers = new ArrayList<>();
+    List<Observer> mObservers = new ArrayList<>();
 
     abstract void notifyObservers();
 
     public synchronized void addObservers(Observer observer) {
-        if (!mObjservers.contains(observer))
-            mObjservers.add(observer);
+        if (!mObservers.contains(observer))
+            mObservers.add(observer);
     }
 }
